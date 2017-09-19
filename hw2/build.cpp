4ce40d0c744c45b6abe400426e509a393d0c3b57
Jason Warta <jasonwarta@gmail.com>
2017-09-19 12:51:38 -0800

    added handling for base case

--- hw2/build.cpp
+++ hw2/build.cpp
@@ -1,5 +1,13 @@
 #include "build.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 int build(int w, int e, const std::vector<Bridge> & bridges) {
+	if (!w || !e || !bridges.size()) return 0;
+	
+	return bridges.front()[2] + build(w, e, std::vector<Bridge>(bridges.begin()+1,bridges.end()));
+
 	return 0;
 }
