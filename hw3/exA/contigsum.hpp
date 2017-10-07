cdf3862778c6c32eb0bc73574183c3d4900682b3
Jason Warta <jasonwarta@gmail.com>
2017-10-06 20:04:10 -0800

    added basic structure of hw3

---
+++ hw3/exA/contigsum.hpp
@@ -0,0 +1,9 @@
+#ifndef CONTIGSUM_HPP_INCLUDED
+#define CONTIGSUM_HPP_INCLUDED
+
+template<typename RAIter>
+int contigSum(RAIter first, RAIter last){
+	return 0;
+}
+
+#endif
---
+++ hw3/exB/inversions.hpp
@@ -0,0 +1,9 @@
+#ifndef INVERSIONS_HPP_INCLUDED
+#define INVERSIONS_HPP_INCLUDED
+
+template<typename RAIter>
+size_t inversions(RAIter first, RAIter last){
+	
+}
+
+#endif
